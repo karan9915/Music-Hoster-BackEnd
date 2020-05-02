485952bca9529aadd48e7cf1d92b795fb8956e57
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-05-02 17:41:29 +0530

    Update SignupBusinessServiceImpl.java

--- musichoster-service/src/main/java/com/upgrad/musichoster/service/business/SignupBusinessServiceImpl.java
+++ musichoster-service/src/main/java/com/upgrad/musichoster/service/business/SignupBusinessServiceImpl.java
@@ -4,6 +4,7 @@ import com.upgrad.musichoster.service.dao.UserDao;
 import com.upgrad.musichoster.service.entity.UserEntity;
 import com.upgrad.musichoster.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,12 +19,37 @@ public class SignupBusinessServiceImpl implements SignupBusinessService {
 
 	@Autowired private PasswordCryptographyProvider passwordCryptographyProvider;
 
+//	public boolean createUser(UserEntity userEntity) throws SignUpRestrictedException{
+//		if( (userDao.createUser(userEntity) != null)){
+//			throw new SignUpRestrictedException("SGR-001","Try any other Username, this Username has already been taken");
+//		}
+//		else {
+//			return true;
+//		}
+//	}
+
+	public boolean getUserByEmail(String email) throws SignUpRestrictedException{
+		if((userDao.getUserByEmail(email) != null)){
+			throw new SignUpRestrictedException("SGR-002","This user has already been registered, try with any other emailId");
+		}
+		else {
+			return true;
+		}
+	}
+
+
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED)
 	public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException{
-		String regex = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(userEntity.getEmail());
+//		String regex = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+//		Pattern pattern = Pattern.compile(regex);
+//		Matcher matcher = pattern.matcher(userEntity.getEmail());
+//		userEntity.setSalt(regex);
+//		userEntity.setPassword(regex);
+		String[] encrypt = passwordCryptographyProvider.encrypt(userEntity.getPassword());
+		userEntity.setSalt(encrypt[0]);
+		userEntity.setPassword(encrypt[1]);
+		return userDao.signup(userEntity);
 
 	}
 }
