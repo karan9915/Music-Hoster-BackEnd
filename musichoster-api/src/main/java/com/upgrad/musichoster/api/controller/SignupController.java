5543af86b7ac555a5f8f8db050605b8e2aa1547a
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-05-02 13:59:16 +0530

    Update SignupController.java

--- musichoster-api/src/main/java/com/upgrad/musichoster/api/controller/SignupController.java
+++ musichoster-api/src/main/java/com/upgrad/musichoster/api/controller/SignupController.java
@@ -44,7 +44,6 @@ public class SignupController {
             entity = signupBusinessService.signup(userEntity);
      //  }
         SignupUserResponse userResponse = new SignupUserResponse().id(entity.getUuid()).status("REGISTERED");
-        
-        return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+         return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
     }
 }
